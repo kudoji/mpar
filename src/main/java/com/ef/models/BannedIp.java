cdd74862a6cf4f92093640c85cfc6632d23b5fb9
kudoji <36791178+kudoji@users.noreply.github.com>
2019-01-03 18:04:45 +0300

    BannedIp.java: validation, tests added (feature #76)

--- src/main/java/com/ef/models/BannedIp.java
+++ src/main/java/com/ef/models/BannedIp.java
@@ -4,6 +4,8 @@
 package com.ef.models;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "banned_ips")
@@ -12,12 +14,14 @@ public class BannedIp {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
+    @NotNull(message = "Ip object cannot be null")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false)
-    private Ip ip;
+    private final Ip ip;
 
+    @Size(min = 5, max = 150, message = "Reason is invalid")
     @Column(nullable = false)
-    private String reason;
+    private final String reason;
 
     public BannedIp(Ip ip, String reason){
         if (ip == null) throw new IllegalArgumentException("Ip object cannot be null");
