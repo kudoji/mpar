c47b607269d43274a4caa62e19a6e2ee9e9d0c14
kudoji <36791178+kudoji@users.noreply.github.com>
2019-01-03 10:41:10 +0300

    tests for BannedIp entity (feature #63)

--- src/test/java/com/ef/models/IpTest.java
+++ src/test/java/com/ef/models/IpTest.java
@@ -3,9 +3,7 @@
  */
 package com.ef.models;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -13,9 +11,11 @@ import javax.persistence.Persistence;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class IpTest {
     private static EntityManagerFactory emf;
+    private EntityManager em;
 
     @BeforeClass
     public static void initialization(){
@@ -27,29 +27,55 @@ public class IpTest {
         emf.close();
     }
 
-    @Test
-    public void writeDataToDB(){
-        EntityManager em = emf.createEntityManager();
+    @Before
+    public void beforeTest(){
+        em = emf.createEntityManager();
         em.getTransaction().begin();
+    }
 
+    @After
+    public void afterTest(){
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Test
+    public void testIpPersistence(){
         final String ipAddress = "127.0.1.1";
         Ip ip = new Ip(ipAddress);
 
         em.persist(ip);
-        em.getTransaction().commit();
-
-        em.getTransaction().begin();
 
         List<Ip> ips = em.createQuery("select ip from Ip as ip", Ip.class).getResultList();
+        assertFalse(ips.isEmpty());
 
-        assertEquals(1, ips.size());
-        assertEquals(ipAddress, ips.get(0).getIp());
+        ip = em.find(Ip.class, ip.getId());
+        assertEquals(ipAddress, ip.getIp());
+        assertEquals(0, ip.getBannedIps().size());
 
         ip = em.find(Ip.class, 1);
         assertEquals(ipAddress, ip.getIp());
+    }
 
-        em.getTransaction().commit();
+    @Test
+    public void testBannedIpsPersistence(){
+        final String ipAddress = "127.0.0.1";
+        final String reason = "banned due to test";
 
-        em.close();
+        Ip ip = new Ip(ipAddress);
+
+        BannedIp bannedIp = new BannedIp(ip, reason);
+        ip.getBannedIps().add(bannedIp);
+
+        em.persist(ip);
+//        em.persist(bannedIp);
+
+        Ip ip1 = em.find(Ip.class, ip.getId());
+
+        assertEquals(1, ip1.getBannedIps().size());
+        assertEquals(reason, ip1.getBannedIps().iterator().next().getReason());
+
+        List<BannedIp> bannedIps = em.createQuery("from BannedIp", BannedIp.class).getResultList();
+        assertFalse(bannedIps.isEmpty());
     }
 }
