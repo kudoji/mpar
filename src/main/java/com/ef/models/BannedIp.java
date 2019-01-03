667a6095d78e5dace526de25751ef63bfe4ddffe
kudoji <36791178+kudoji@users.noreply.github.com>
2019-01-03 11:43:13 +0300

    BannedIp: illegal arguments are not allowed (feature #63)

--- src/main/java/com/ef/models/BannedIp.java
+++ src/main/java/com/ef/models/BannedIp.java
@@ -20,6 +20,9 @@ public class BannedIp {
     private String reason;
 
     public BannedIp(Ip ip, String reason){
+        if (ip == null) throw new IllegalArgumentException("Ip object cannot be null");
+        if (reason.isEmpty()) throw new IllegalArgumentException("Reason cannot be empty");
+
         this.ip = ip;
         this.reason = reason;
     }
--- src/test/java/com/ef/models/BannedIpTest.java
+++ src/test/java/com/ef/models/BannedIpTest.java
@@ -3,6 +3,19 @@
  */
 package com.ef.models;
 
+import org.junit.Test;
+
 public class BannedIpTest {
-    //  covered in IpTest.java
+    //  majority covered in IpTest.java
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorWithIllegalIp(){
+        BannedIp bannedIp = new BannedIp(null, "reason");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorWithIllegalReason(){
+        Ip ip = new Ip();
+        BannedIp bannedIp = new BannedIp(ip, "");
+    }
 }
